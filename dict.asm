78bf85df2a0b592ef87f500b607a9295deed518d
UserNameIsAlredyTaken <gdaniltatarinof@gmail.com>
2017-09-19 10:18:12 +0300

    добавил комментарии

--- dict.asm
+++ dict.asm
@@ -7,19 +7,20 @@ find_word:
 	.loop:
 		push rsi
 		push rdi
-		add rsi, 8
-		call string_equals
+		add rsi, 8 ; rsi указывал на метку предыдущего элемента, переходим на ключ
+		call string_equals ; сравниваем ключ с тем, что у нас лежит в word_buffer
 		pop rdi
-		pop rsi
+		pop rsi		
+		test rax, rax	; проверяем, результат функции string_equals
 		
-		test rax, rax
 		jnz .finish
-		mov rsi, [rsi]
-		test rsi, rsi
+		mov rsi, [rsi] ; помещаем в rsi ссылку на предыдущий элемент
+		test rsi, rsi ; проверяем, не равна ли эта ссылка 0
 		jnz .loop 
-		xor rax, rax
+		xor rax, rax ;если равна, возвращаем 0
 		ret
-	.finish:
+		
+	.finish: ; возвращаем указатель на метку искомой записи
 		mov rax, rsi	
 	ret
 	
--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@
 
 section .data
 
-error_msg: db 'Sorry, but no :(', 10
+error_msg: db 'Sorry, but no :(', 10 ;сообщение в случае не нахождения элемента
 
 section .text
 
@@ -15,33 +15,33 @@ global _start
 
 _start:
 	.loop:
-		call read_string
-		mov rdi, rax
-		mov rsi, link
-		call find_word
-		test rax,rax
+		call read_string ; читаем строку с stdin до символа переноса строки
+		mov rdi, rax ; помещаем в rdi указатель на word_buffer с введённой строкой
+		mov rsi, link ; помещаем в rsi указатель на последний элемент списка
+		call find_word ; вызываем find_word
+		test rax,rax ; поверяем вывод find_word на 0
 	
-		je .error
+		je .error ; если 0, значит слово не найдено
 	
-		mov rdi, rax
-		add rdi, 8
-		call string_length
-		add rdi, rax
-		inc rdi
-		call print_string	
+		mov rdi, rax ; помещаем в rdi указатель на метку ескомой записи
+		add rdi, 8 ; перемещаем указатель на ключ искомой записи
+		call string_length ; считываеем количество символов в ключе
+		add rdi, rax ; перемещаем указатель на конец ключа (0 нультерминированной строки)
+		inc rdi ; помещаем указатель на начало значения записи
+		call print_string ; выводим значение
 	
 .end:
-	mov rax, 60
+	mov rax, 60 ; завершаем работу программы
 	xor rdi, rdi
 	syscall
 	
 .error:
-	mov rax, 1
+	mov rax, 1 ; выводим сообщение об ошибке
 	mov rdi, 2
 	mov rsi, error_msg
 	mov rdx, 43
 	syscall
 	
-	jmp .end
+	jmp .end 
 	
 	
